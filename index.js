edc5716132e1ba8ca28fd4d7598471e5f653f895
n1rjal <nirjalpaudel54312@gmail.com>
2021-10-02 19:39:59 +0545

    added CRUD operations

--- index.js
+++ index.js
@@ -10,9 +10,26 @@ const typeDefs = gql`
   type Query {
     books: [Book!]!
   }
+
+  input BookInfo {
+    title: String
+    author: String
+    pages: Int
+  }
+
+  input BookUpdateInfo {
+    book_id: Int
+    bookInfo: BookInfo
+  }
+
+  type Mutation {
+    addBook(bookInfo: BookInfo!): Book!
+    updateBook(bookUpdateInfo: BookUpdateInfo): Book
+    deleteBook(id: Int): Boolean
+  }
 `;
 
-const books = [
+let books = [
   {
     title: "The Awakening",
     author: "Kate Chopin",
@@ -29,9 +46,41 @@ const resolvers = {
   Query: {
     books: () => books,
   },
+  Mutation: {
+    addBook: (parent, { bookInfo }, context) => {
+      const { author, title, pages } = bookInfo;
+      books.push({ author, title, pages });
+      return { author, title, pages };
+    },
+
+    updateBook: (parent, { bookUpdateInfo }, context) => {
+      const {
+        book_id,
+        bookInfo: { author, title, pages },
+      } = bookUpdateInfo;
+      books[book_id] = { author, title, pages };
+      return books[book_id];
+    },
+
+    deleteBook: (parent, { id }, context) => {
+      try {
+        if (book[id]) {
+          throw "NotFoundError";
+        }
+        books = books.filter((book, index) => index !== id);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    },
+  },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({ req, res }),
+});
 server.listen().then(({ url }) => {
   console.log("Listening to port " + url);
 });
